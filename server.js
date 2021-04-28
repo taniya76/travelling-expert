b4777f110d5596b18a2c4bc328fea2e1ce86f268
taniya76 <www.taniyamittalhgn@gmail.com>
2021-04-28 20:20:51 +0530

    change

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const async = require('express-async-errors')
-const fetch = require('node-fetch')
+const async = require('express-async-errors');
+const fetch = require('node-fetch');
+const request = require("request");
+const https = require("https");
 
 /* Server Setup */
 const app = express();
 app.use(cors());
-
-app.use(express.static(__dirname+'public'));
-
+app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({ extended: true }));
 // parse application/json
 app.use(express.json());
 
@@ -25,7 +24,7 @@ app.listen(process.env.PORT || Port, () => {
 /* Routes */
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(__dirname+ "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post('/save', (req, res, next) => {
